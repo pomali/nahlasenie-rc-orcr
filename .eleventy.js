27f8672d43ef2a0c38fc5a142f813b9b915c733d
pom <>
2022-06-24 23:01:56 +0200

    fix layout, add step2

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,14 @@
+const eleventySass = require("eleventy-sass");
+
 module.exports = function (eleventyConfig) {
   require("dotenv").config();
   eleventyConfig.addGlobalData("env", process.env);
+  eleventyConfig.addPlugin(eleventySass);
+  eleventyConfig.addPassthroughCopy({
+    "node_modules/govuk-frontend/govuk/assets/images": "assets/images",
+    "node_modules/govuk-frontend/govuk/assets/fonts": "assets/fonts",
+    "node-modules/govuk-frontend/govuk/*.js": "js/",
+  });
 
   // Set custom directories for input, output, includes, and data
   return {
